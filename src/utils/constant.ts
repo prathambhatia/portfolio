35876a28abdacce2abbd2e9fc54ca3e1d458c1a7
Pratham Bhatia <prathambhatia741@gmail.com>
2026-05-04 01:39:46 +0530

    Fix company logos for DevX AI Labs and Dun & Bradstreet

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -3,7 +3,7 @@ import { I_Education, I_Experience } from "@/types/project";
 export const experienceData: I_Experience[] = [
     {
         company_link: "https://www.devxlabs.ai/",
-        company_logo: "/stealth-startup.jpeg",
+        company_logo: "/devx-logo.png",
         company_name: "DevX AI Labs",
         duration: "2024 - present",
         job_title: "Software Engineer",
@@ -11,7 +11,7 @@ export const experienceData: I_Experience[] = [
     },
     {
         company_link: "https://www.dnb.co.in/",
-        company_logo: "/100xdevs.jpeg",
+        company_logo: "/dnb-logo.svg",
         company_name: "Dun & Bradstreet",
         duration: "2023 - 2024",
         job_title: "Software Engineer Intern",
